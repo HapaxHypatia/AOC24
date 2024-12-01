c704e6f7a72cf4d96ab2098f0650c6f85c772a2f
HapaxHypatia <r.haskmann@gmail.com>
2024-12-01 22:28:14 +1000

    initial commit day 1 complete

---
+++ 011224.py
@@ -0,0 +1,13 @@
+# list1 = [3,4,2,1,3,3]
+# list2 = [4,3,5,3,9,3]
+
+with open("1.txt", 'r') as file:
+	data = [x.split() for x in file.readlines()]
+	list1 = sorted([int(line[0]) for line in data])
+	list2 = sorted([int(line[1]) for line in data])
+
+result1 = sum(max(list1[i], list2[i]) - min(list1[i], list2[i]) for i in range(1000))
+print(result1)
+
+result2 = sum(a * sum(1 for b in list2 if b == a) for a in list1)
+print(result2)
